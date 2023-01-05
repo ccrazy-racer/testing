4550a0cce696f1128083cf97c4779c345c94bc8c
Amogh <55197119+ccrazy-racer@users.noreply.github.com>
2023-01-05 18:16:43 +0530

    Update verifyResults.py

--- verifyResults.py
+++ verifyResults.py
@@ -4,6 +4,5 @@ import warnings
 
 file = open("results.sarif", 'r')
 for i in file:
-    print(i.lstrip())
     if i.lstrip().startswith('"id":'):
         exit("ERROR: There are security issues, please check security section!")
