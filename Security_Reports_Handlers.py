587af72deaf22d8af9018148cec951f62039cb38
Amogh <55197119+ccrazy-racer@users.noreply.github.com>
2023-01-09 13:10:31 +0530

    Update Security_Reports_Handlers.py

--- Security_Reports_Handlers.py
+++ Security_Reports_Handlers.py
@@ -1,5 +1,4 @@
 import os
-import warnings
 from datetime import date
 from zipfile import ZipFile
 import shutil
@@ -9,7 +8,7 @@ for i in os.listdir():
         filename = i
         file = open(i, 'r')
         break
-       
+
 direc = "Security_Reports"
 date = str(date.today())
 
@@ -17,7 +16,7 @@ path_direc = os.path.join(direc)
 cur_direc = os.path.join(direc, date)
 
 if os.path.exists(path_direc):
-    
+
     # This checks for pervious day unzipped folders, if any then zips that folder.
     list_dirs = os.listdir(path_direc)
     if len(list_dirs) != 0:
@@ -25,11 +24,19 @@ if os.path.exists(path_direc):
             if len(j) != 0 and j.endswith('.zip') == False:
                 if j.split('.')[0] != date:
                     name = j
-                    name += '.zip'
-                    with ZipFile(os.path.join(direc, name), 'w') as zip_object:
-                        for files in os.listdir(os.path.join(direc, j)):
-                            zip_object.write(os.path.join(direc, name, files))
-                            
+                    zip_name = shutil.make_archive(
+                        name, 'zip', os.path.join(direc, name))
+                    try:
+                        shutil.move(zip_name, os.path.join(
+                            direc))
+                    except PermissionError:
+                        print("Permission denied.")
+
+                    try:
+                        shutil.rmtree(os.path.join(direc, name))
+                    except OSError as error:
+                        print(error)
+
     if os.path.exists(cur_direc):
         try:
             shutil.copyfile(filename, os.path.join(direc, date, filename))
